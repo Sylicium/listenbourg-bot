e9d7ce46cb89d713d04d217f325853581382e5ef
Sylicium <96630108+Sylicium@users.noreply.github.com>
2022-11-08 18:39:23 +0100

    Disabled command devices

--- bot/slashcommands/devices.js
+++ bot/slashcommands/devices.js
@@ -13,7 +13,14 @@ module.exports = {
             description: "Affiche les appareils sur lequels l'utilisateur est connecté.",
             dmPermission: false,
             type: Discord.ApplicationCommandType.ChatInput,
-            options: []
+            options: [
+                {
+                    "name": "user",
+                    "description": "L'utilisateur à regarder",
+                    "type": Discord.ApplicationCommandOptionType.User,
+                    "required": true,
+                }
+            ]
         },
         canBeDisabled: false,
         permisionsNeeded: {
@@ -22,7 +29,7 @@ module.exports = {
         },
         rolesNeeded: [],
         superAdminOnly: false,
-        disabled: false,
+        disabled: true,
         indev: false,
         hideOnHelp: true
     },
@@ -30,40 +37,23 @@ module.exports = {
 
         await interaction.deferReply()
 
-        let guildInfos = await Modules.Database.isReferencedGuild(interaction.guild.id)
+        let the_user = await bot.users.fetch(interaction.options.getUser("user").id)
 
-        let ico = {
-            on: config.emojis.bluebutton.tag,
-            off: config.emojis.whitebutton.tag,
-            bar_l: config.emojis.whitebarleft.tag,
-            bar: config.emojis.whitebar.tag,
-            bar_r: config.emojis.whitebarright.tag,
-        }
-
+        console.log("the_user",the_user)
+        console.log("the_user.presence",the_user.presence)
         let lines = [
-            `${ico.bar_l}${ico.bar}${ico.bar}${ico.bar} **Bot** ${ico.bar}${ico.bar}${ico.bar}${ico.bar_r}`,
-            `${ico.off} Maintenance`,
-            ``,
-            `${ico.bar_l}${ico.bar}${ico.bar}${ico.bar} **Serveur** ${ico.bar}${ico.bar}${ico.bar}${ico.bar_r}`,
-            `${guildInfos ? ico.on : ico.off} Référencé`,
-            `${(guildInfos && guildInfos.settings.certified) ? ico.on : ico.off} Certifié`,
-            `${(guildInfos && guildInfos.settings.private) ? ico.on : ico.off} Mode privé`,
-            `${ico.on} Relèvement des statistiques (à venir)`,
-            `${(guildInfos && guildInfos.settings.isBotOnGuild) ? ico.on : ico.off} Le bot est toujours présent sur le serveur`,
-            `Description: \`\`\`${guildInfos ? guildInfos.guild.description : "Serveur non référencé"}\`\`\` `,
-            `Mots clé: \`\`\`${(guildInfos && guildInfos.keywords) ? guildInfos.keywords.join(", ") : "Aucun mot clé"}\`\`\` `,
-            ``,
-            `${ico.bar_l}${ico.bar}${ico.bar}${ico.bar} **Autre** ${ico.bar}${ico.bar}${ico.bar}${ico.bar_r}`,
-            `Aucun paramètre.`
+            `Web: ${the_user.presence?.clientStatus?.web}`,
+            `Mobile: ${the_user.presence?.clientStatus?.mobile}`,
+            `Desktop: ${the_user.presence?.clientStatus?.desktop}`,
         ].join("\n")
 
         await interaction.editReply({
             embeds: [
                 new Discord.EmbedBuilder()
-                    .setTitle("Informations générales")
+                    .setTitle("Informations sur les appareils")
                     .setColor("FFFFFF")
                     .setDescription(lines)
-                    .setFooter({ text: `Recensement des discord Listenbourgeois` })
+                    .setTimestamp()
             ]
         })
         return;
