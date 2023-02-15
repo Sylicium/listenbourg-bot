8b077cb8c0f0a3072d1e143c0b5cd6457fe3550f
Sylicium <96630108+Sylicium@users.noreply.github.com>
2023-02-15 20:39:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -547,9 +547,6 @@ function _allCode() {
                 }
             }
             let the_value = (noNormalCharCount/total)
-            console.log("noNormalCharCount:",noNormalCharCount)
-            console.log("total:",total)
-            console.log("the_value:",the_value)
             if( the_value > 0.05 ) return true
             return false
         }
